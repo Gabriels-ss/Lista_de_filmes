5e7a26266aeb2e5d10d789eb2684bea164ebeba1
Gabriel Sousa sabino <133471641+Gabriels-ss@users.noreply.github.com>
2023-07-28 06:43:38 -0300

    Add files via upload
    Um projeto em que você pode pesquisar filmes pelo nome e irá procurar numa API web, retornando o nome do filme e o pôster.

---
+++ projeto_filmes/js/script.js
@@ -0,0 +1,15 @@
+/*
+function resolverAfter25econds() {
+    return new Promise(resolver => {
+        setTimeout(() => {
+        resolver('resolves')
+          {   2000}})
+    })
+}
+async  function asyncCall() {
+    console.log('caling');
+
+    const result = await resolverAfter25econds()
+    console.log(result);
+}
+*/
